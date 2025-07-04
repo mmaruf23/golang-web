a3411759cf28486b5d0b75aa454686eef7284e70
Muhammad Ma'ruf <maruuf82@gmail.com>
2025-07-04 07:24:04 +0700

    http test

---
+++ http_test.go
@@ -0,0 +1,26 @@
+package golangweb
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Test")
+}
+
+func TestHelloHandler(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hello", nil)
+	recorder := httptest.NewRecorder()
+
+	HelloHandler(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+	fmt.Println(response.StatusCode)
+	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
