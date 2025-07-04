5772682fd9d11aa36941634c992778d6ddbf5da2
Muhammad Ma'ruf <maruuf82@gmail.com>
2025-07-04 19:41:35 +0700

    query param

--- http_test.go
+++ http_test.go
@@ -13,7 +13,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func TestHelloHandler(t *testing.T) {
-	request := httptest.NewRequest("GET", "http://localhost:8080/hello", nil)
+	request := httptest.NewRequest("GET", "http://localhost:8080/test", nil)
 	recorder := httptest.NewRecorder()
 
 	HelloHandler(recorder, request)
@@ -22,5 +22,27 @@ func TestHelloHandler(t *testing.T) {
 	body, _ := io.ReadAll(response.Body)
 	fmt.Println(response.StatusCode)
 	fmt.Println(response.Status)
+	fmt.Println(string(body))
+}
+
+func BilangHalo(w http.ResponseWriter, r *http.Request) {
+	nama := r.URL.Query().Get("nama")
+
+	if nama == "" {
+		fmt.Fprint(w, "Halo banh")
+	} else {
+		fmt.Fprintf(w, "Halo bang %s", nama)
+	}
+}
+
+func TestBilangHalo(t *testing.T) {
+	request := httptest.NewRequest("GET", "http://localhost:8080/hallo?nama=Nama", nil)
+	recorder := httptest.NewRecorder()
+
+	BilangHalo(recorder, request)
+
+	response := recorder.Result()
+	body, _ := io.ReadAll(response.Body)
+
 	fmt.Println(string(body))
 }
