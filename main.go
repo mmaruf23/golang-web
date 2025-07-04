4ef8d4f9eae7540a0d8852e5eb7dc6764516e4b5
Muhammad Ma'ruf <maruuf82@gmail.com>
2025-07-04 07:13:14 +0700

    request

--- main.go
+++ main.go
@@ -7,17 +7,14 @@ import (
 
 func main() {
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/products", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "All Products")
-	})
-	mux.HandleFunc("/products/1", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Product with id = 1")
-	})
+	var handler http.HandlerFunc = func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, r.Method)
+		fmt.Fprint(w, r.RequestURI)
+	}
 
 	server := http.Server{
 		Addr:    "localhost:8080",
-		Handler: mux,
+		Handler: handler,
 	}
 
 	server.ListenAndServe()
